51a27613fbb9dcfb2faadc6b8d128b86d295f8a6
Saurabh <sauravthakulla683@gmail.com>
2025-10-31 23:00:44 +0530

    Merge updated

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -1,17 +1,22 @@
 let arr = [2, 5, 6];
 let arr2 = [1, 3, 4, 8];
-let add = new Array(arr.length + arr2.length);
+let merge = new Array(arr.length + arr2.length);
+let i = 0;
+let j = 0;
+let k = 0;
 
-function merge(arr, arr2) {
-  let k = 0;
-  let i = 0;
-  for (s = 0; i < add.length; s++) {
-    if (a[i] <= a[j]) {
-      add[s] = a[i];
-      i++;
-    } else {
-      add[s] = a[j];
-      j++;
-    }
+while (i < arr.length && j < arr2.length) {
+  if (arr[i] < arr2[j]) {
+    merge[k++] = arr[i++];
+  } else {
+    merge[k++] = arr2[j++];
   }
 }
+while (j < arr2.length) {
+  merge[k++] = arr2[j++];
+}
+while (i < arr.length) {
+  merge[k++] = arr[i++];
+}
+
+console.log(merge);
