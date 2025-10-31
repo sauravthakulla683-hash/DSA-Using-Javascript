0a5983691a484d4c5d40fa8f7ab5d72fd89a0e77
Saurabh <sauravthakulla683@gmail.com>
2025-10-31 23:07:21 +0530

    reuploaded

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -1,9 +1,7 @@
 let arr = [2, 5, 6];
 let arr2 = [1, 3, 4, 8];
+let k = (j = i = 0);
 let merge = new Array(arr.length + arr2.length);
-let i = 0;
-let j = 0;
-let k = 0;
 
 while (i < arr.length && j < arr2.length) {
   if (arr[i] < arr2[j]) {
@@ -12,11 +10,10 @@ while (i < arr.length && j < arr2.length) {
     merge[k++] = arr2[j++];
   }
 }
-while (j < arr2.length) {
-  merge[k++] = arr2[j++];
-}
 while (i < arr.length) {
   merge[k++] = arr[i++];
 }
-
+while (j < arr2.length) {
+  merge[k++] = arr2[j++];
+}
 console.log(merge);
