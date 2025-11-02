7fc649d66ade202b9a4526e46a862c1b246dd141
Saurabh <sauravthakulla683@gmail.com>
2025-11-02 23:47:09 +0530

    Loading animation practise

---
+++ js_advance/js.js
@@ -0,0 +1,17 @@
+let a = document.querySelector(".child");
+let button = document.querySelector(".button");
+let b = 10;
+let c = false;
+
+button.addEventListener("click", () => {
+  if (!c) {
+    s = setInterval(() => {
+      a.style.width = b + "%";
+      b = b + 0.1;
+    }, 5);
+    c = !c;
+  } else {
+    clearInterval(s);
+    c = !c;
+  }
+});
