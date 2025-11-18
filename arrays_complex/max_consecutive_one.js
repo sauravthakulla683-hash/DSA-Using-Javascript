02b772f30ed272a0451c1550d9642d1b6c1014ea
Saurabh <sauravthakulla683@gmail.com>
2025-11-18 23:24:08 +0530

    fixes

--- arrays_complex/max_consecutive_one.js
+++ arrays_complex/max_consecutive_one.js
@@ -1,9 +1,13 @@
 let a = [1, 2, 3, 1, 1, 1, 1, 1];
 let count = 0;
+let max = 0;
 
 for (let i = 0; i < a.length; i++) {
   if (a[i] === 1) {
     count++;
+    max = Math.max(count, max);
+  } else {
+    count = 0;
   }
 }
-console.log(count);
+console.log(max);
